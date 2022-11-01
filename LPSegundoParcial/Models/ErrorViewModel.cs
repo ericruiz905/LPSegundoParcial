7024929c647fe805f7123ac0a8d709d9b6af9b5e
ericr <ericr@DESKTOP-JM920J5>
2022-11-01 04:16:11 -0400

    Add project files.

---
+++ LPSegundoParcial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LPSegundoParcial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
